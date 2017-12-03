a8e49e913da291d5f900f2a7aba4303d06eb8471
Bryan Mutai <mutaiwork@gmail.com>
2017-12-03 00:11:25 +0000

    Update download.sh

--- subcitydatabase/download.sh
+++ subcitydatabase/download.sh
@@ -1,3 +1,3 @@
-wget https://www.dropbox.com/s/9h9e3msehdrjeww/subcity.sql?dl=0
+wget https://www.dropbox.com/s/khw6f8r4cm8kmz5/subcity.sql?dl=0
 mv subcity.sql?dl=0%0D subcity.sql
 sed '1 s/^/USE subcity;\n/' subcity.sql? > /docker-entrypoint-initdb.d/subcity.sql
