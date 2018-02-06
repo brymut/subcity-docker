209d67e9e3e5e844d0aa14212c787a49d95018fd
brymut <mutaiwork@gmail.com>
2018-02-06 19:36:50 +0000

    okay this time

--- subcitydatabase/download.sh
+++
@@ -1,3 +0,0 @@
-wget https://www.dropbox.com/s/khw6f8r4cm8kmz5/subcity.sql?dl=0
-mv subcity.sql?dl=0%0D subcity.sql
-sed '1 s/^/USE subcity;\n/' subcity.sql? > /docker-entrypoint-initdb.d/subcity.sql
